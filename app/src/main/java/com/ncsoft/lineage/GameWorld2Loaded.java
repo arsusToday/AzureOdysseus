aac79309206e55c61e0dbc91a1481759c9341821
arsusToday <arslan.ochkhaev@yandex.ru>
2022-10-25 12:13:23 +0600

    Java New v1 Done

--- app/src/main/java/com/ncsoft/lineage/GameWorld2Loaded.java
+++ app/src/main/java/com/ncsoft/lineage/GameWorld2Loaded.java
@@ -3,9 +3,15 @@ package com.ncsoft.lineage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class GameWorld2Loaded extends AppCompatActivity {
 
@@ -26,6 +32,12 @@ public class GameWorld2Loaded extends AppCompatActivity {
         powerScore++;
         if (powerScore > 5){
             textView.setText("Great Job, Friend!");
+            final Handler handler = new Handler(Looper.getMainLooper());
+            handler.postDelayed(() -> {
+                startActivity(new Intent(getApplicationContext(), GameHubWorld1.class));
+                finish();
+            }, 3500);
+            Snackbar.make(view, "Try it again!", BaseTransientBottomBar.LENGTH_LONG).show();
 
         }
     }
